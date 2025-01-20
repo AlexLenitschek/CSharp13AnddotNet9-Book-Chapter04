2e8a45dcded7ea66ffb71c3a75211f7967b80fb1
alex lenitschek <Alexander.lenitschek@gmx.de>
2025-01-20 17:51:01 +0100

    Adding more Exercises of this chapter.

---
+++ CallStackExceptionHandling/Program.cs
@@ -0,0 +1,29 @@
+﻿using CallStackExceptionHandlingLib; // To use Processor.
+using static System.Console;
+
+WriteLine("In Main");
+Alpha();
+
+void Alpha()
+{
+    WriteLine("In Alpha");
+    Beta();
+}
+
+void Beta()
+{
+    try
+    {
+        WriteLine("In Beta");
+        Processor.Gamma();
+    }
+    catch (Exception ex)
+    {
+        WriteLine($"Caught this: {ex.Message}");
+        //throw ex; // Acts as if the exception happened in Beta.
+        //throw; // This on the other hand includes all the information of the call stack.
+
+        // This is the best way to throw an exception as it is like throw + a personalized message.
+        throw new Exception("Something went wrong in Beta", ex);
+    }
+}
